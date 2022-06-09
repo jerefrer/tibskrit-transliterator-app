ad8e826b13fbb211ae1b90bd3aac4d57c3eed1de
Jérémy FRERE <frere.jeremy@gmail.com>
2022-06-09 09:24:11 +0200

    Adds a test (not yet passing) to handle ter tsheks after a bhyoh

---
+++ tests/transliteration/others.js
@@ -0,0 +1,10 @@
+testGroups.push({
+  name: "Other cases",
+  sentences: true,
+  tests: [
+    {
+      tibetan: "ཨཿཀཿསཿམཿརཿཙཿཤཿདཿརཿསཿམཿརཿཡཿཕཊ༔ བཛྲ་སེང་ཧ་མུ་ཁ་བྷྱོ༔ རཏྣ་སེང་ཧ་མུ་ཁ་བྷྱོ༔ པདྨ་སེང་ཧ་མུ་ཁ་བྷྱོ༔ ཀརྨ་སེང་ཧ་མུ་ཁ་བྷྱོ༔",
+      result: "aḥ kaḥ saḥ maḥ raḥ caḥ śaḥ daḥ raḥ saḥ maḥ raḥ yaḥ phaṭ    vajra sengha mukha bhyoḥ    ratna sengha mukha bhyoḥ    padma sengha mukha bhyoḥ    karma sengha mukha bhyoḥ"
+    },
+  ]
+})
